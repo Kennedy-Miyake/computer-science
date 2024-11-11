66397ecb4dd56c7f8d26ba02483d0b6ebf2fba9d
Kennedy Miyake <kennedykaiak57@gmail.com>
2024-11-11 08:42:14 -0300

    deletado o arquivo LinkedList.cpp e adicionado as funções ao arquivo .hpp II

--- data-structures/libs/linked-list/LinkedList.hpp
+++ data-structures/libs/linked-list/LinkedList.hpp
@@ -13,7 +13,36 @@ public:
     Node(T data);
 
     void setData(T data);
-    T getData() { return m_data; }
+    T getData() const { return m_data; }
 };
+
+template <typename T>
+class LinkedList {
+private:
+    std::unique_ptr<Node<T>> m_head_node{};
+    int m_size{0};
+
+public:
+    LinkedList(T data);
+    
+    T info_data() const;
+};
+
+// Implementações da classe Node
+template <typename T>
+Node<T>::Node(T data) : m_data{data} {}
+
+template <typename T>
+void Node<T>::setData(T data) { m_data = data; }
+
+// Implementações da classe LinkedList
+template <typename T>
+LinkedList<T>::LinkedList(T data)
+    : m_head_node{std::make_unique<Node<T>>(data)} {}
+
+template <typename T>
+T LinkedList<T>::info_data() const {
+    return m_head_node->getData();    
+}
 
 #endif
