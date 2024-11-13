ad0370b933bf83da2494986b1586d20091ca01b7
Kennedy Miyake <kennedykaiak57@gmail.com>
2024-11-13 06:38:28 -0300

    adicionado a função de adicionar um nó (sem testes)

--- data-structures/libs/linked-list/LinkedList.hpp
+++ data-structures/libs/linked-list/LinkedList.hpp
@@ -14,6 +14,7 @@ public:
 
     void setData(T data);
     T getData() const { return m_data; }
+    std::shared_ptr<Node<T>> getNextNode() const { return m_ptr_to_next_node; }
 };
 
 template <typename T>
@@ -26,6 +27,7 @@ public:
     LinkedList(T data);
     
     T info_data() const;
+    void addNode(T data);
 };
 
 // Implementações da classe Node
@@ -43,6 +45,19 @@ LinkedList<T>::LinkedList(T data)
 template <typename T>
 T LinkedList<T>::info_data() const {
     return m_head_node->getData();    
+}
+template <typename T>
+void LinkedList<T>::addNode(T data) {
+    std::shared_ptr<Node<T>> new_node{std::make_shared<Node<T>>(data)};
+    std::shared_ptr<Node<T>> temp_node = m_head_node;
+    while(temp_node->getNextNode() != nullptr) {
+        temp_node = temp_node->getNextNode();
+    }
+    auto point_to = [&new_node, &temp_node]() -> void {
+        auto aux_node = temp_node->getNextNode();
+        aux_node = new_node;
+    }
+    point_to();
 }
 
 #endif
