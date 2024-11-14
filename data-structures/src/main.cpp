b889215fd3654e7338c6ee2b85d5b164ca54a631
Kennedy Miyake <kennedykaiak57@gmail.com>
2024-11-14 05:49:13 -0300

    adicionando uma nova função para adicionar nós por índice

--- data-structures/src/main.cpp
+++ data-structures/src/main.cpp
@@ -4,9 +4,10 @@
 int main() {
 
     LinkedList ll{10};
-
     ll.addNode(20);
-    std::cout << ll.info_data() << '\n';
+    ll.addNode(30);
+    ll.addNode(3, 15);
+    ll.info_data();
 
     return 0;
 }
